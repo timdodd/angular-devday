3684c48f0292035562c5453cb5cd64707cba23fd
Unknown <timbododd@hotmail.com>
2019-01-31 21:33:52 -0600

    feat: 6 navigate to detail, display image, next and previous

--- xkcd-app/src/app/xkcd-detail/xkcd-detail.component.ts
+++ xkcd-app/src/app/xkcd-detail/xkcd-detail.component.ts
@@ -1,16 +1,42 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {XkcdService} from "../services/xkcd.service";
+import {Comic} from "../models/comic";
 
 @Component({
   selector: 'app-xkcd-detail',
   templateUrl: './xkcd-detail.component.html',
   styleUrls: ['./xkcd-detail.component.css']
 })
-export class XkcdDetailComponent implements OnInit {
+export class XkcdDetailComponent {
 
-  constructor() {
+  comic: Comic;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private xkcdService: XkcdService) {
+    this.route.params.subscribe(params => {
+      this.loadComicById(params['id']);
+    })
   }
 
-  ngOnInit() {
+  next() {
+    this.navigate(this.comic.num + 1)
+  }
+
+  previous() {
+    this.navigate(this.comic.num - 1)
+  }
+
+  private navigate(id: id) : void {
+    this.router.navigateByUrl("/xkcd/" + id);
+
+  }
+
+  private loadComicById(id: number): void {
+    this.xkcdService.getComicById(id).subscribe(comic =>
+      this.comic = comic
+    );
   }
 
 }
--- xkcd-app/src/app/xkcd-list/xkcd-list.component.ts
+++ xkcd-app/src/app/xkcd-list/xkcd-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {XkcdService} from "../services/xkcd.service";
 import {Comic} from "../models/comic";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-xkcd-list',
@@ -11,12 +12,17 @@ export class XkcdListComponent implements OnInit {
 
   comics: Comic[];
 
-  constructor(private xkcdService: XkcdService) { }
+  constructor(private xkcdService: XkcdService,
+              private router: Router) { }
 
   ngOnInit() {
     this.xkcdService.findComics().subscribe(
       comics => this.comics = comics
     );
+  }
+
+  onSelectComic(comic: Comic) {
+    this.router.navigateByUrl('/xkcd/' + comic.num);
   }
 
 }
