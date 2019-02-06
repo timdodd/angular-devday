d819a032385f8f65269a44e631ac91509e9d1120
Unknown <timbododd@hotmail.com>
2019-02-05 21:13:15 -0600

    feat: 7 ngModel creating a table filter.

--- xkcd-app/src/app/app.module.ts
+++ xkcd-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {HeaderThreeComponent} from './header-three/header-three.component';
 import {XkcdListComponent} from './xkcd-list/xkcd-list.component';
 import {XkcdDetailComponent} from './xkcd-detail/xkcd-detail.component';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- xkcd-app/src/app/xkcd-list/xkcd-list.component.ts
+++ xkcd-app/src/app/xkcd-list/xkcd-list.component.ts
@@ -10,19 +10,36 @@ import {Router} from '@angular/router';
 })
 export class XkcdListComponent implements OnInit {
 
-  comics: Comic[];
+  private comics: Comic[] = [];
+  filteredComics: Comic[] = [];
+  filter: string;
 
   constructor(private xkcdService: XkcdService,
-              private router: Router) { }
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.xkcdService.findComics().subscribe(
-      comics => this.comics = comics
-    );
+      comics => {
+        this.comics = comics;
+        this.filterComics();
+      });
+  }
+
+  onChangeFilter() {
+    this.filterComics();
   }
 
   onSelectComic(comic: Comic) {
     this.router.navigateByUrl('/xkcd/' + comic.num);
+  }
+
+  private filterComics(): void {
+    if (!this.filter) {
+      this.filteredComics = this.comics;
+    } else {
+      this.filteredComics = this.comics.filter(comic => comic.title.toLowerCase().includes(this.filter.toLowerCase()));
+    }
   }
 
 }
